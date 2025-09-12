e93b2c36ac448b30f437dd1db67b17cf3a0af105
ak-nika <akingbayiojo@gmail.com>
2025-09-12 14:23:59 +0100

    W03 Individual Activity: Expand Inventory complete

---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { getParam } from "./utils.mjs";
+
+const category = getParam("category");
+const dataSource = new ProductData();
+const myList = new ProductList(
+  category,
+  dataSource,
+  document.querySelector(".product-list"),
+);
+
+myList.init();
